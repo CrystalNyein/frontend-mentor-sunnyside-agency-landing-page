d27571317840ceb50cba62acc78cca1e0a6c8208
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-02 23:24:45 +0630

    Mobile menu works

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,56 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useRef, useState } from "react";
 import logo from "../assets/images/logo.svg";
 import "./NavBar.css";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <div className="NavBar">
-        <div className="nav-logo">
-          <img src={logo} alt="Sunnyside Logo"></img>
-        </div>
-        <div className="nav">
-          <i className="fas fa-hamburger nav-icon-mobile"></i>
-          <div className="nav-group">
-            <ul>
-              <li className="nav-link barlow-font">About</li>
-              <li className="nav-link barlow-font">Services</li>
-              <li className="nav-link barlow-font">Projects</li>
-              <li className="nav-link fraunces-font">Contact</li>
-            </ul>
-          </div>
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navGroup = useRef(null);
+  const nav = useRef(null);
+  const menuBtn = useRef(null);
+  const toggleMenu = () => {
+    if (nav.current.classList.contains("active")) {
+      nav.current.classList.remove("active");
+      setMenuOpen(false);
+    } else {
+      nav.current.classList.add("active");
+      setMenuOpen(true);
+    }
+  };
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!menuBtn.current.contains(e.target)) {
+        if (navGroup.current && !navGroup.current.contains(e.target)) {
+          toggleMenu();
+        }
+      }
+    };
+    if (menuOpen) window.addEventListener("click", handleClickOutside, true);
+    return () => {
+      window.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+  return (
+    <div className="NavBar">
+      <div className="nav-logo">
+        <img src={logo} alt="Sunnyside Logo"></img>
+      </div>
+      <div className="nav" ref={nav}>
+        <i
+          ref={menuBtn}
+          className="fas fa-hamburger nav-icon-mobile"
+          onClick={toggleMenu}
+        ></i>
+        <div className="nav-group" ref={navGroup}>
+          <ul>
+            <li className="nav-link barlow-font">About</li>
+            <li className="nav-link barlow-font">Services</li>
+            <li className="nav-link barlow-font">Projects</li>
+            <li className="nav-link fraunces-font">Contact</li>
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default NavBar;
