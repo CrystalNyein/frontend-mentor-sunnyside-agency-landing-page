b05168e8b880ac0322128c9baaae14437b14e545
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-03 07:56:42 +0630

    Move hr to outside

--- src/components/LearnMoreSec.js
+++ src/components/LearnMoreSec.js
@@ -7,9 +7,8 @@ const LearnMoreSec = (props) => {
       <div className={props.sequence + " learn-more-text "}>
         <h2 className="title fraunces-font">{props.title}</h2>
         <p className="barlow-font">{props.para}</p>
-        <button className="btn fraunces-font">
-          Learn More<hr style={{ backgroundColor: props.color }}></hr>
-        </button>
+        <button className="btn fraunces-font">Learn More</button>
+        <hr style={{ backgroundColor: props.color }}></hr>
       </div>
       <div
         className="learn-more-img"
